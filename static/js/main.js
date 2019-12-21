2f84bc58e75a2bab3de649a7ba5e8e11aad452e4
Nakajima-Kuro <tnnamanh@gmail.com>
2019-12-21 14:21:24 +0700

    a

--- static/js/main.js
+++ static/js/main.js
@@ -5,22 +5,22 @@ window.onload = function () {
     done = true;
     $('html, body').animate({ scrollTop: '0px' }, 200);
     setTimeout(function () {
-        $('#cover_6').addClass('fade-in-fast')
+        $('#cover_6').addClass('fade-in-fast') //background
     }, 0 + this.trigger);
     setTimeout(function () {
-        $('#cover_2').addClass('fade-in')
+        $('#cover_2').addClass('fade-in') //girl
     }, 500 + this.trigger);
     setTimeout(function () {
-        $('#cover_1').addClass('fade-in');
-    }, 1000 + this.trigger);
+        $('#cover_1').addClass('fade-in'); //Piano
+    }, 900 + this.trigger);
     setTimeout(function () {
-        $('#cover_3').addClass('fade-in')
-    }, 1800 + this.trigger);
+        $('#cover_3').addClass('fade-in') //Sol
+    }, 1500 + this.trigger);
     setTimeout(function () {
-        $('#cover_4').addClass('fade-in')
-    }, 2500 + this.trigger);
+        $('#cover_4').addClass('fade-in')   //Title
+    }, 2400 + this.trigger);
     setTimeout(function () {
-        $('#cover_5').addClass('fade-in-slow')
+        $('#cover_5').addClass('fade-in-medium') //Punji
     }, 3000 + this.trigger);
 }
 $(document).ready(function () {
@@ -41,6 +41,7 @@ $(document).ready(function () {
                 $song1.addClass('fade-in-fast');
                 setTimeout(function () {
                     $('#cover_jacket').addClass('fade-in-medium')
+                    $('#blue_arrow').addClass('fade-in')
                 }, 300 + this.trigger);
                 setTimeout(function () {
                     $('#song2').addClass('fade-in-medium')
@@ -69,7 +70,7 @@ $(document).ready(function () {
                 setTimeout(function () {
                     $('#song10').addClass('fade-in-medium')
                 }, 1500 + this.trigger);
-            }, { offset: '70%' });
+            }, { offset: '80%' });
         }, 500);
     });
 })
