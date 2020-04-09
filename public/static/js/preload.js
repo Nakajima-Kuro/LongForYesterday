04edf3c78a5be837caae232ab988dc7c8f337e65
Nakajima-Kuro <tnnamanh@gmail.com>
2020-04-09 15:12:27 +0700

    test

--- public/static/js/preload.js
+++ public/static/js/preload.js
@@ -1,7 +1,7 @@
 // lock scroll position, but retain settings for later
 var scrollPosition = [
-    self.pageXOffset || document.documentElement.scrollLeft || document.body.scrollLeft,
-    self.pageYOffset || document.documentElement.scrollTop || document.body.scrollTop
+    self.pageXOffset || document.documentElement.scrollLeft,
+    self.pageYOffset || document.documentElement.scrollTop
 ];
 
 var html = jQuery('html'); // it would make more sense to apply this to body, but IE7 won't have that
